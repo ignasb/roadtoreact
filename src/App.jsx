82fd853c2ed79e0458cde1e36d85888e22accb28
Ignas Burlingis <ignas.burlingis@gmail.com>
2022-12-20 17:21:45 +0200

    Fetching hacker news

--- src/App.jsx
+++ src/App.jsx
@@ -19,11 +19,6 @@ const initialStories = [
   },
 ];
 
-const getAsyncStories = () =>
-  new Promise((resolve) =>
-    setTimeout(() => resolve({ data: { stories: initialStories } }), 2000),
-  );
-
 const storiesReducer = (state, action) => {
   switch (action.type) {
     case "STORIES_FETCH_INIT":
@@ -69,6 +64,8 @@ const useStorageState = (key, initialState) => {
   return [value, setValue];
 };
 
+const API_ENDPOINT = "https://hn.algolia.com/api/v1/search?query=";
+
 const App = () => {
   const [searchTerm, setSearchTerm] = useStorageState("search", "React");
 
@@ -81,21 +78,16 @@ const App = () => {
   React.useEffect(() => {
     dispatchStories({ type: "STORIES_FETCH_INIT" });
 
-    getAsyncStories()
+    fetch(`${API_ENDPOINT}react`)
+      .then((response) => response.json())
       .then((result) => {
         dispatchStories({
           type: "STORIES_FETCH_SUCCESS",
-          payload: result.data.stories,
+          payload: result.hits,
         });
       })
-      .catch(() => {
-        dispatchStories({
-          type: "STORIES_FETCH_FAILURE",
-        });
-      });
+      .catch(() => dispatchStories({ type: "STORIES_FETCH_FAILURE" }));
   }, []);
-
-  React.useEffect(() => {}, []);
 
   const handleRemoveStory = (item) => {
     dispatchStories({
